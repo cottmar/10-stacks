0cf3092914b338a1c46e9698779d90e3e61caf31
cara ottmar <cara.ottmar@gmail.com>
2018-04-27 17:51:33 -0700

    data structure built and added queue.js and stack.js

---
+++ src/lib/stack.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Node = require('./node');
+
+module.exports = class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  pushToTop(value) {
+    const node = new Node(value);
+
+    node.next = this.top;
+    this.top = node;
+    return this;
+  }
+
+  popOffTop() {
+    this.top = this.top.next;
+    return this;
+  }
+
+  peek() {
+    return this.top.value;
+  }
+
+  isEmpty() {
+    if (this.top === null) {
+      return true;
+    }
+    return false;
+  }
+}
